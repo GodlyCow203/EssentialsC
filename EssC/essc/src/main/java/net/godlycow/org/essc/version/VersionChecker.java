5668d400161b7e1fa72ce1465864a7f6aadc8d76
Godly Cow <cowandfilbert@gmail.com>
2026-04-04 17:30:28 +0200

    add /tphereall + 4.1.3

--- EssC/essc/src/main/java/net/godlycow/org/essc/version/VersionChecker.java
+++ EssC/essc/src/main/java/net/godlycow/org/essc/version/VersionChecker.java
@@ -21,9 +21,7 @@ public class VersionChecker implements Listener {
     private static final String MODRINTH_PROJECT_ID = "K7HZMVgx";
     private static final String MODRINTH_API_URL = "https://api.modrinth.com/v2/project/" + MODRINTH_PROJECT_ID + "/version";
     private static String latestVersion = "unknown";
-    private static String latestMajor = "unknown";
     private static boolean updateAvailable = false;
-    private static boolean breakingChange = false;
     private static final MiniMessage MINI = MiniMessage.miniMessage();
     private static final Pattern VERSION_PATTERN = Pattern.compile("\"version_number\"\\s*:\\s*\"([^\"]+)\"");
 
@@ -63,20 +61,8 @@ public class VersionChecker implements Listener {
 
                     if (matcher.find()) {
                         latestVersion = matcher.group(1);
-                        String[] versionParts = latestVersion.split("\\.");
-                        latestMajor = versionParts.length > 0 ? versionParts[0].replaceAll("[^0-9]", "") : "0";
-
                         String currentVersion = plugin.getDescription().getVersion();
-                        String[] currentParts = currentVersion.split("\\.");
-                        String currentMajorRaw = currentParts.length > 0 ? currentParts[0] : "0";
-                        String currentMajor = currentMajorRaw.replaceAll("[^0-9]", "");
-
                         updateAvailable = !currentVersion.equalsIgnoreCase(latestVersion);
-
-                        int latestMajorNum = latestMajor.matches("\\d+") ? Integer.parseInt(latestMajor) : 0;
-                        int currentMajorNum = currentMajor.matches("\\d+") ? Integer.parseInt(currentMajor) : 0;
-
-                        breakingChange = updateAvailable && latestMajorNum > currentMajorNum;
 
                         new BukkitRunnable() {
                             @Override
@@ -115,14 +101,6 @@ public class VersionChecker implements Listener {
             plugin.getLogger().info("Current: " + currentVersion);
             plugin.getLogger().info("Latest: " + latestVersion);
             plugin.getLogger().info("Download: https://modrinth.com/plugin/essentialsc");
-
-            if (breakingChange) {
-                plugin.getLogger().warning("--------------------------------");
-                plugin.getLogger().warning("BREAKING CHANGES DETECTED!");
-                plugin.getLogger().warning("Major version change requires manual migration.");
-                plugin.getLogger().warning("Please backup before upgrading!");
-                plugin.getLogger().warning("--------------------------------");
-            }
         } else {
             plugin.getLogger().info("EssentialsC - Up To Date");
             plugin.getLogger().info("--------------------------------");
@@ -142,22 +120,13 @@ public class VersionChecker implements Listener {
         Bukkit.getScheduler().runTaskLater(EssentialsC.getInstance(), () -> {
             if (!player.isOnline()) return;
 
-            player.sendMessage(MINI.deserialize("<dark_gray>━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</dark_gray>"));
-            player.sendMessage(MINI.deserialize("<gradient:#00D2FF:#3A7BD5>EssentialsC Update Available</gradient>"));
-            player.sendMessage(MINI.deserialize("<dark_gray>━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</dark_gray>"));
-            player.sendMessage(MINI.deserialize("<gray>Current: <red>" + EssentialsC.getInstance().getDescription().getVersion() + "</red>"));
-            player.sendMessage(MINI.deserialize("<gray>Latest: <green>" + latestVersion + "</green>"));
-            player.sendMessage(MINI.deserialize("<gray>Download: <aqua><click:open_url:'https://modrinth.com/plugin/essentialsc'><hover:show_text:'<gray>Click to open Modrinth'>https://modrinth.com/plugin/essentialsc</hover></click></aqua>"));
-
-            if (breakingChange) {
-                player.sendMessage(MINI.deserialize("<dark_red>━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</dark_red>"));
-                player.sendMessage(MINI.deserialize("<bold><color:#FF0040>⚠ BREAKING CHANGES DETECTED!</color></bold>"));
-                player.sendMessage(MINI.deserialize("<color:#FF6B6B>This update contains breaking changes.</color>"));
-                player.sendMessage(MINI.deserialize("<color:#FF6B6B>Backup your server before upgrading!</color>"));
-                player.sendMessage(MINI.deserialize("<dark_red>━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</dark_red>"));
-            }
-
-            player.sendMessage(MINI.deserialize("<dark_gray>━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</dark_gray>"));
+            player.sendMessage(MINI.deserialize("<color:#AAAAAA>━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</color>"));
+            player.sendMessage(MINI.deserialize("<color:#FFF200>EssentialsC Update Available</color>"));
+            player.sendMessage(MINI.deserialize("<color:#AAAAAA>━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</color>"));
+            player.sendMessage(MINI.deserialize("<color:#AAAAAA>Current: <color:#FF0000>" + EssentialsC.getInstance().getDescription().getVersion() + "</color></color>"));
+            player.sendMessage(MINI.deserialize("<color:#AAAAAA>Latest: <color:#00AA00>" + latestVersion + "</color></color>"));
+            player.sendMessage(MINI.deserialize("<color:#AAAAAA>Download: <color:#66AAFF><click:open_url:'https://modrinth.com/plugin/essentialsc'><hover:show_text:'<color:#AAAAAA>Click to open Modrinth'>https://modrinth.com/plugin/essentialsc</hover></click></color></color>"));
+            player.sendMessage(MINI.deserialize("<color:#AAAAAA>━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━</color>"));
         }, 20L);
     }
 
@@ -167,9 +136,5 @@ public class VersionChecker implements Listener {
 
     public static boolean isUpdateAvailable() {
         return updateAvailable;
-    }
-
-    public static boolean isBreakingChange() {
-        return breakingChange;
     }
 }
