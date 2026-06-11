1fefbff724c8e8750ba32c7c47ead635ad3c6467
Godly Cow <cowandfilbert@gmail.com>
2026-06-11 19:31:49 +0200

    update faststats

--- EssC/essc/src/main/java/net/godlycow/org/essc/integration/metrics/faststats/FastStatsManager.java
+++ EssC/essc/src/main/java/net/godlycow/org/essc/integration/metrics/faststats/FastStatsManager.java
@@ -1,20 +1,25 @@
 package net.godlycow.org.essc.integration.metrics.faststats;
 
-import dev.faststats.bukkit.BukkitMetrics;
-import dev.faststats.core.Metrics;
+import dev.faststats.Metrics;
+import dev.faststats.bukkit.BukkitContext;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FastStatsManager {
 
-    private Metrics metrics;
-
+    private BukkitContext context;
 
     public void init(JavaPlugin plugin) {
-        metrics = BukkitMetrics.factory()
-                .token("753b5c694c676a97c8966eee8a159012")
-                .create(plugin);
-        Bukkit.getLogger().info("Enabled Faststats Metrics");
+
+        context = new BukkitContext.Factory(plugin, "753b5c694c676a97c8966eee8a159012").metrics(Metrics.Factory::create).create();
+        context.ready();
+
+        Bukkit.getLogger().info("Enabled FastStats Metrics");
     }
 
+    public void shutdown() {
+        if (context != null) {
+            context.shutdown();
+        }
+    }
 }
